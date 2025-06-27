d4d882fe2ae9fc2547580f9491d7fa7577787cfd
Sayhan Ahmed <sayhan@ewnbd.com>
2025-06-28 02:01:14 +0600

    Hero section complete

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,43 +2,55 @@
 
 import Image from "next/image";
 import React, { useState } from "react";
-import image from "@/assets/images/hero-1.png";
-import breakfast from "@/assets/images/hero-2.png";
-import lunch from "@/assets/images/hero-3.png";
-import dinner from "@/assets/images/hero-4.png";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa6";
 
 const Hero = () => {
   const [rotate, setRotate] = useState(0);
-  console.log("🚀 ~ Hero.jsx:12 ~ Hero ~ rotate:", rotate);
+
+  const handleLeft = () => {
+    setRotate((prev) => (prev + 90) % 360);
+  };
+
+  const handleRight = () => {
+    setRotate((prev) => (prev - 90 + 360) % 360);
+  };
 
   const images = [
     {
       id: 2032,
       img: image,
-      style: "absolute -start-2/5 -top-[35%] translate-1/2 z-10",
+      style: `absolute xl:-start-2/5 xl:-top-[35%] xl:translate-1/2 z-10 hidden xl:block xl:w-[650px] ${
+        rotate === 0 && "md:block -top-[20%] -start-1/5 w-[400px] "
+      }`,
       rotate: 0,
     },
     {
       id: 123,
       img: breakfast,
-      style: "absolute -start-[35%] bottom-[18%] translate-1/2 z-10",
+      style: `absolute xl:-start-[35%] xl:bottom-[18%] xl:translate-1/2 z-10 xl:block xl:w-[650px] ${
+        rotate === 90
+          ? "md:block -start-[20%] -bottom-[20%] w-[400px]"
+          : "hidden"
+      } `,
       rotate: 90,
     },
     {
       id: 124,
       img: lunch,
-      style: "absolute end-44 bottom-[20%] translate-1/2 z-10",
+      style: `absolute xl:end-44 xl:bottom-[20%] xl:translate-1/2 z-10 hidden xl:block xl:w-[650px] ${
+        rotate === 180 && "md:block -end-38 -bottom-32 w-[400px]"
+      }`,
       rotate: 180,
     },
     {
       id: 125,
       img: dinner,
-      style: "absolute end-52 -top-[40%] translate-1/2 z-10",
+      style: `absolute xl:end-52 xl:-top-[40%] xl:translate-1/2 z-10 hidden xl:block xl:w-[650px] ${
+        rotate === 270 && "md:block -end-32 -top-32 w-[400px]"
+      }`,
       rotate: 270,
     },
   ];
-
-  // end-44 bottom-[20%]
 
   return (
     <div
@@ -48,15 +60,53 @@ const Hero = () => {
         rotate === 180 && "bg-[#953653]"
       } ${rotate === 270 && "bg-[#296666]"} transition-all`}
     >
-      <div className="px-[88px] relative border h-full w-full z-10 flex items-center">
-        <div className="max-w-[900px] text-white">
-          <h1 className="text-8xl font-normal">BREAKFAST</h1>
-          <p className="text-xl font-bold ml-1 mt-3">
+      <div className="md:px-[88px] px-10 relative border h-full w-full z-10 flex items-center">
+        <div className="max-w-[900px] md:w-[60%] w-full text-white">
+          <h1 className="md:text-8xl text-[45px] font-normal">BREAKFAST</h1>
+          <p className="md:text-xl font-bold ml-1 mt-3">
             Breakfast, often referred to as the ‘most important meal of the
             day’, provides essential nutrients to kick start our day. It
             includes a variety of foods, like fruits, cereals, dairy products,
             and proteins, that contribute to a balanced diet.{" "}
           </p>
+
+          <div className="mt-8 mb-[70px] md:hidden w-full flex justify-between items-center">
+            <div
+              onClick={handleRight}
+              className={`w-[41px] h-[41px] rounded-full flex justify-center items-center ${
+                rotate === 0 && "bg-[#A52A2A]"
+              } ${rotate === 90 && "bg-[#0c3659]"} ${
+                rotate === 180 && "bg-[#a95c68]"
+              } ${rotate === 270 && "bg-[#113333]"}`}
+            >
+              {" "}
+              <FaChevronLeft />
+            </div>
+
+            {images.map((img) => (
+              <Image
+                key={`00${img.id}`}
+                src={img.img}
+                width={270}
+                height={270}
+                className={`${rotate === img.rotate ? "block" : "hidden"}`}
+                alt="Breakfast food"
+              />
+            ))}
+
+            <div
+              onClick={handleLeft}
+              className={`w-[41px] h-[41px] rounded-full flex justify-center items-center ${
+                rotate === 0 && "bg-[#A52A2A]"
+              } ${rotate === 90 && "bg-[#0c3659]"} ${
+                rotate === 180 && "bg-[#a95c68]"
+              } ${rotate === 270 && "bg-[#113333]"}`}
+            >
+              {" "}
+              <FaChevronRight />
+            </div>
+          </div>
+
           <div className="flex items-center gap-5 mt-10">
             {images.map((img) => (
               <div
@@ -71,11 +121,12 @@ const Hero = () => {
                   className={`rotate-[180deg]`}
                   alt="Breakfast food"
                 />
-                {img.rotate === rotate ? (
-                  <hr className="bg-white py-[1.3px] rounded-full mt-4 w-[60%] mx-auto" />
-                ) : (
-                  <hr className="bg-transparent border-transparent py-[1.3px] rounded-full mt-4 w-[60%] mx-auto" />
-                )}
+
+                <hr
+                  className={`py-[1.3px] rounded-full mt-4 w-[60%] mx-auto transition-all duration-700 ${
+                    img.rotate === rotate ? "bg-white" : "border-transparent"
+                  }`}
+                />
               </div>
             ))}
           </div>
@@ -93,19 +144,21 @@ const Hero = () => {
         <div
           className={`w-[60vw] min-w-[437px] h-[60vw] min-h-[437px] absolute bottom-[-30vw] end-[-30vw] rotate-[${rotate}deg] transition-all duration-700`}
         >
-          {images.map((img) => (
-            <Image
-              key={`00${img.id}`}
-              src={img.img}
-              width={630}
-              height={630}
-              className={img.style}
-              alt="Breakfast food"
-            />
-          ))}
+          <div className="md:block hidden">
+            {images.map((img) => (
+              <Image
+                key={`00${img.id}`}
+                src={img.img}
+                width={630}
+                height={630}
+                className={img.style}
+                alt="Breakfast food"
+              />
+            ))}
+          </div>
 
           <div
-            className={`w-full h-full rounded-full rotate-[-52.22deg]  ${
+            className={`w-full h-full rounded-full rotate-[-52.22deg] ${
               rotate === 0 && "bg-[#A52A2A]"
             } ${rotate === 90 && "bg-[#0c3659]"} ${
               rotate === 180 && "bg-[#a95c68]"
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,16 +5,16 @@ import React from "react";
 const Navbar = () => {
   return (
     <nav className="absolute z-50 w-full">
-      <header className="z-20 top-0 md:mx-[60px] my-[50px] flex justify-between">
-        <Link href={""} className="text-[32px] text-white font-bold">
+      <header className="z-20 top-0 md:mx-[60px] md:my-[50px] my-7 flex justify-between gap-10">
+        <Link href={""} className="text-[32px] text-white font-bold hidden md:block">
           RESTAURANT
         </Link>
 
-        <div className="relative lg:w-[821px] bg-white rounded-[20px]">
-          <Search className="absolute top-1/2 -translate-y-1/2 ml-7" />
+        <div className="relative lg:w-[821px] bg-white md:rounded-[20px] rounded-[7px] mx-[18px] md:mx-0 w-full transition-all">
+          <Search className="absolute top-1/2 -translate-y-1/2 ml-7 max-md:w-[16px]" />
           <input
             type="text"
-            className="w-full h-[61px] pr-7 ps-24 placeholder:text-2xl placeholder:font-semibold placeholder:text-[#2D2D2D]"
+            className="w-full md:h-[61px] h-[35px] pr-7 md:ps-24 ps-14 md:placeholder:text-2xl md:placeholder:font-semibold placeholder:text-[#2D2D2D]"
             placeholder="Search...."
           />
         </div>
